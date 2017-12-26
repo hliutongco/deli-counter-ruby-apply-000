5f0b6a99ed7ebd51834b787263c934fdee58113b
Helen Liutongco <bellpickle@gmail.com>
2017-12-26 22:41:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  line_string = ""
+  if katz_deli == []
+    "The line is currently empty."
+  else
+    katz_deli.each_with_index do |person, index|
+      line_string += "#{index+1}. #{person}"
+    end
+    line_string
+  end
+end
